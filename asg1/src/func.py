41dcf43667cd121d9cb841d6b528739b2c4d21ef
Shuagib Ibrahim <shuagibibrahim@hotmail.com>
2026-03-31 16:33:22 +0200

    Update func.py

--- asg1/src/func.py
+++ asg1/src/func.py
@@ -1,8 +1,8 @@
 import numpy as np 
 import matplotlib.pyplot as plt
+import torch as tp
 
-
-#test = np.linspace(0,20,20)
+test = np.linspace(0,20,20)
 
 #def func_euclidean2(x: np.ndarray):
 """Function Lp Norm 2  collapsing over the coloums and taking the sum of each next point and current.
@@ -10,9 +10,10 @@ import matplotlib.pyplot as plt
     #return np.sum(np.sum(np.pow(x[1:] - x[:-1],2)))
 
 
-def func_eclidean1(x:np.array):
+def func_eclideannorm(x:np.array):
    """ This function is the Lp norm 2 and takes the difference  squared and sums 
-   them for all x_n points in the trajectory"""
+   them for all x_n points in the trajectory
+   Takes a vector as input and Returns a scalar"""
    result = 0
    N = len(x)
    for i in range(N-1):
@@ -20,6 +21,15 @@ def func_eclidean1(x:np.array):
       squared = difference**2
       result += np.sum(squared)
    return result 
-        
+   
 
 
+def gradient_eclideannorm(x:np.array):
+   """ Take the gradient of Lp 2 norm. Uses Autograd from Torch.
+   Takes the function as input and returns its derivate with respect to x"""
+   grad = tp.autograd.grad(func_eclideannorm)
+   return grad(x)
+
+
+def func_smooth(D,x):
+   
