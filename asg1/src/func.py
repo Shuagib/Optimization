b47a3e006de6d64162eef502176b34d59933a789
Shuagib Ibrahim <shuagibibrahim@hotmail.com>
2026-03-31 11:11:15 +0200

    Update func.py

--- asg1/src/func.py
+++ asg1/src/func.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def func_euclidean(x: np.ndarray):
-    """Function Lp Norm 2  Summing over the coloums and taking the sum of each next point and current.   """
-    return np.sum(np.pow(x[1:] - x[:-1]),2,axis=1)
+    """Function Lp Norm 2  collapsing over the coloums and taking the sum of each next point and current.
+       This creates A vector of size (n-1,2)  """
+    return np.sum(np.sum(np.pow(x[1:] - x[:-1],2),axis=1))
+
+
 
