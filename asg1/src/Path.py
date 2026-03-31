a24719ad5103044ae7aa38d39c604fbdba0103fb
Shuagib Ibrahim <shuagibibrahim@hotmail.com>
2026-03-31 18:04:17 +0200

    Fixed alot and maybe done with first part needs testing
    yeyyee

--- asg1/src/Path.py
+++ asg1/src/Path.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 #Choose start and Goal
 start_point_x,start_point_y = (0.0,0.0)
-end_point_x,end_point_y = (22.4,22.4)
+end_point_x,end_point_y = (19.0,22.0)
 
 #Amount of Points 
 N = 20
@@ -12,13 +12,16 @@ N = 20
 x_axis = np.linspace(start_point_x,end_point_y,N)
 y_axis = np.linspace(start_point_x,end_point_y,N)
 
+# Creating path as an (n,2) Array
+path = np.column_stack((x_axis,y_axis))
+
 #Creating Circular Obstacles (More Ecliples then circles)
-Ob1 = plt.Circle(( 19.0 , 19.0), 1,color="darkorange",zorder=2)
-Ob2 = plt.Circle(( 5.0 , 5.0), 1,color="darkorange", zorder= 2)
+Ob1 = plt.Circle(( 16.0 , 19.0), 1,color="darkorange",zorder=2)
+Ob2 = plt.Circle(( 6.0 , 7.0), 1,color="darkorange", zorder= 2)
 
 #Plotting the Initial path, start and end points and Obstacles
 fig, ax = plt.subplots()
-ax.plot(x_axis,y_axis,'o',color="orange")
+ax.plot(path,'o',color="orange")
 ax.set_title("Initial Path and Obstacles")
 ax.add_patch(Ob1)
 ax.add_patch(Ob2)
--- asg1/src/func.py
+++ asg1/src/func.py
@@ -1,8 +1,11 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 import torch as tp
+import math as ma
 
-test = np.linspace(0,20,20)
+a = np.linspace(0,20,20)
+b = np.linspace(0,20,20)
+n2_array = np.column_stack((a,b))
 
 #def func_euclidean2(x: np.ndarray):
 """Function Lp Norm 2  collapsing over the coloums and taking the sum of each next point and current.
@@ -31,20 +34,37 @@ def gradient_eclideannorm(x:np.array):
    return grad(x)
 
 
-#Creating the datastructurer of a circle, we need its position and radius
+#Creating the datastructurer of a circle. its position (x,y) and radius (r) ((x,y),r)
 
-ob = [(( 19.0 , 19.0), 1), (( 5.0 , 5.0), 1)]
-#print(len(ob)) has the size of two
+ob = [(( 16.0 , 19.0), 3.14), (( 6.0 , 7.0), 2.19)]
 
 
-def detector(x:np.array,cir):
+
+def detector(x:np.array,obj):
    """ Helper function which measure the distance between obstacle
-   and tracjectory point"""
-
-   return np.linalg.norm(x - cir[0])
-    
-      
+   and tracjectory point. Since our object is a tuple, we are only interested at (x,y) coordinates
+                           """
+   return np.linalg.norm(x - obj)
 
 
-def Obstacle_func(x:np.array):
-   pass 
+
+
+def pentality_func(x:np.array, obj):
+   """ Obstacle model that add penality large penality if we are close the obstacles and small if we are far away.
+         Returns a penality score """
+   N = len(x) # The lenght of the matrix
+   penalty = 0 #Pentalty
+   for y in obj: #Loop through each tuple
+      r  = y[1] #Get the radius
+      i = 0 # Control variable 
+      while i < N: #Continues running as long we are i are smaller then the lenght
+         dis = detector(x[i],y)  #Creating penalty givn each trajcectory and object
+         if dis > r: #First condtion measure how if we are far enough away
+            dis_diff_pow = np.pow(dis-r,2)  #add square it
+            penalty += 1/dis_diff_pow #Add small penalty since it since we are far awy
+         else:
+            penalty += ma.inf #Reaches the obstacle need large penality
+         i += 1 
+   return penalty
+         
+
