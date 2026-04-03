5b32fac41e032fba387f0e6283b11484f7388a0c
Shuagib Ibrahim <shuagibibrahim@hotmail.com>
2026-04-03 15:22:47 +0200

    Updated Quasi Newton method with DFP
    Fixed alot of things and had fun

--- asg1/src/quasi.py
+++ asg1/src/quasi.py
@@ -1,4 +1,6 @@
-from objective_func import gradient_objectivefunc, objective_function
+import torch.autograd as ta
+import numpy as np
+from objective_func import gradient_objectivefunc, objective_function,gradient_objective,objective_function
 import numpy as np
 from strong_brack import strong_backtracking
 class DescentMethod:
@@ -7,21 +9,63 @@ class DescentMethod:
 
 class Quasi_NewtonMethod(DescentMethod):
     """ Using Quasi Newton Method which it approximate the hessian and goes in the steepest descent direction """
-    def __init__(self,x:np.array,alpha:float,lam: float,mu:float,obj,grad):
+    def __init__(self,x_flat, n, x_start, x_goal,alpha ,lam,mu,obj,grad,D,f):
         self.alpha = alpha
         self.lam = lam
         self.mu = mu
-        self.x = x
+        self.x_flat = x_flat
+        self.n = n
+        self.x_start = x_start
+        self.x_goal = x_goal
         self.obj = obj
         self.grad = grad
-        self.I = np.identity(2,dtype = float)
+        self.D = D
+        self.f = f
+        self.Q =  np.identity(2*(self.n-2),dtype = float)  #Defining the identity matrix so it fits a path 2(n-2)
+        self.g = gradient_objective(self.x_flat, self.n, self.x_start, self.x_goal, self.D, self.obj, self.lam, self.mu)
+        self.func = lambda x: objective_function(x, self.n, self.x_start, self.x_goal, self.D, self.obj, self.lam, self.mu) #Creating one function so it's easier to pass since strongbracket uses f(x+alpha*d)
+        self.nabla = lambda y: gradient_objective(y, self.n, self.x_start, self.x_goal, self.D, self.obj, self.lam, self.mu)
+
         if lam  < 0 or mu < 0:
             raise ValueError("Error: Must be strictly bigger than 0")
         
-    def step(self):
-        H_I = self.I #Defining the identity matrix
-        Q, g = H_I, self.grad
-        #self.x, alpha = strong_backtracking
+    def DFP(self):
+        """ Using Davidon-Fletcher-Powell (DFP) method """
+        search_direction = -self.Q @ self.nabla(self.x_flat) #Computing the search direction
+        alpha_lo, rejected, tried_alpha = strong_backtracking(func,nabla,self.x_flat,search_direction,self.alpha) #Getting the fist alpha
+        new_x = self.x_flat + alpha_lo *search_direction
+        delta  = new_x - self.x_flat
+        gamma  = self.nabla(new_x) - self.nabla(self.x_flat)
+        Q_new = (self.Q - np.outer(self.Q @ gamma, self.Q @ gamma) / np.dot(gamma, self.Q @ gamma) + np.outer(delta, delta) / np.dot(delta, gamma))   #approximating our matrix using np.outer since we are creating matrix and np.dot to create scalars
+        self.Q = Q_new 
+        self.x_flat = new_x 
+        return self.x_flat, alpha_lo,rejected,tried_alpha
+    
+    def opt_1(self,kmax=10,ep=0.001):
+        x_point = []
+        f_value = []
+        k = 0
+        while np.linalg.norm(self.g) > ep: #Convergences rate
+            f_value.append(self.func(self.x_flat)) #Function values
+            x_point.append(self.x_flat.copy()) #Keeping track of all the tracjectories
+            self.x_flat, alpha = self.DFP()
+            self.g = self.nabla(self.x_flat)
+            k +=1 
+            #We are running 10 iteration as standard, 
+            if k >= kmax:
+                break 
+        return self.x_flat, alpha, x_point,f_value
+
+
+
+
+            
+
+
+
+
+
+
 
 
 
