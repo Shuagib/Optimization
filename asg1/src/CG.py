Gradient descent and CG fixed

asg1/src/CG.py
@@ -32,19 +32,19 @@ class Conjugate_Gradient(DescentMethod):
         grad_func = lambda y: gradient_objective(y, self.n, self.start, self.goal, self.D, self.obj, self.lam, self.mu)
         func = lambda x: objective_function(x,self.n, self.start , self.goal ,self.D,self.obj,self.lam,self.mu)
         grad = gradient_objective(self.x, self.n, self.start, self.goal, self.D, self.obj, self.lam, self.mu) #NewGradient
-        d = -grad #Gradient direction
-        alpha_z, alpha_tried, alpha_rejected = strong_backtracking(func,grad_func,self.x,d,self.alpha) #Using Stron_backtracking as line search
         beta_PR =  np.dot(np.transpose(grad), (grad - self.gradient)) / np.dot(np.transpose(self.gradient), self.gradient) 
         beta_FR = max(beta_PR,0)
+        self.d = - grad  + beta_FR * self.d
+        alpha_z, alpha_tried, alpha_rejected = strong_backtracking(func,grad_func,self.x, self.d,self.alpha) #Using Stron_backtracking as line searc
+        updat_x = self.x + alpha_z * self.d
         self.gradient = grad #Old gradient
-        updat_x = self.x + alpha_z * d
-        self.x = updat_x 
-        self.d = - self.gradient  + beta_FR * self.d
+        self.x = updat_x
+       
          #Updating rule  for next position x 
          #Polak-Ribière Method for keeping track of the difference between our Gradient directions
          #New descnt direction given our negative gradient, difference  (Beta) and old descent direction
                                      #Using Strong_Bracketing Line search search for finding best alpha
-        return updat_x, alpha_z,grad,alpha_tried, alpha_rejected
+        return self.x, alpha_z,grad,alpha_tried, alpha_rejected
     
     def opt(self, kmax=100):
         """ Optimizer for using Conjugate method to find optimial design point"""
@@ -57,14 +57,19 @@ class Conjugate_Gradient(DescentMethod):
             func_values.append(objective_function(self.x,self.n, self.start , self.goal ,self.D,self.obj,self.lam,self.mu)) #Function value
             x_points.append(self.x.copy()) #current Positions append in a list 
             updat_x, alpha_z,grad, alpha_tried, alpha_rejected = self.step()
-            print(f"The current next step is: {updat_x}")
+            #print(f"The current next step is: {updat_x}")
             alpha_list.append(alpha_z)
-            print(f"The current Alphaz list is: {alpha_list}")
-            print(f'The gradient is: {grad}')
+            #print(f"The current Alphaz list is: {alpha_list}")
+            #print(f'The gradient is: {grad}')
+            print(np.linalg.norm(grad),"CG norm gradient")
+            #print(f"alpha that has been searched:  {alpha_tried}")
+            #print(f"Alphat that been rejected: {alpha_rejected}")
+            #print(f'The points we have benn: {x_points}')
+
             if np.linalg.norm(grad) < ep: #Termination Condition
-                print(f'The Gradient is: {grad} and is absolute value is { np.linalg.norm(grad)}, Has convedged yet: {np.linalg.norm(grad) < ep} away from converging')
+                #print(f'The Gradient is: {grad} and is absolute value is { np.linalg.norm(grad)}, Has convedged yet: {np.linalg.norm(grad) < ep} away from converging')
                 break 
             k +=1
             print(f"The iteration is: {k}") 
-        return updat_x, alpha_list,x_points,func_values,alpha_tried, alpha_rejected #Return current position, alphas list, x position, function values
+        return  updat_x , alpha_list,x_points,func_values,alpha_tried, alpha_rejected #Return current position, alphas list, x position, function values
     

asg1/src/GD.py
@@ -33,7 +33,7 @@ class GradientDescent(DescentMethod):
         d = -grad                   
         alpha = backtracking_line_search(self.f, grad_func, self.x, d, self.alpha)
         self.x = self.x - alpha * grad
-        return self.x, alpha, grad
+        return self.x , alpha, grad
     
 
     def opt(self,iter=100):
@@ -49,10 +49,11 @@ class GradientDescent(DescentMethod):
             f_values.append(objective_function(self.x,self.n, self.start , self.goal ,self.D,self.obj,self.lam,self.mu))
             x_poins.append(self.x.copy())
             x_old = self.x.copy()
-            step_x, alphaz ,grad = self.step()
-            print(grad)
-            print(self.x)
-            print(alphaz, "This is a alpha")
+            step_x, alphaz, grad = self.step()
+            #print(grad,"GD gradient")
+            print(np.linalg.norm(grad),"GD norm gradient")
+            #print(self.x)
+            #print(alphaz, "This is a alpha")
             alphz.append(alphaz)
             stepz.append(step_x)
             f_new = objective_function(self.x,self.n, self.start , self.goal ,self.D,self.obj,self.lam,self.mu)
@@ -61,7 +62,7 @@ class GradientDescent(DescentMethod):
                 print(f"f_old: {f_old}, f_new: {f_new}, diff: {f_old - f_new}")
                 break
             k +=1 
-            print(alphz)
+            #print(alphz)
         return self.x, x_poins, f_values,alphz, stepz
 
 