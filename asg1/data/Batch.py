ca7a35bb14d0eac8af3c61a064887b5fba47f2f8
Shuagib Ibrahim <shuagibibrahim@hotmail.com>
2026-04-14 21:52:33 +0200

    adasds

--- asg1/data/Batch.py
+++ asg1/data/Batch.py
@@ -208,7 +208,7 @@ print(fc12_params[1].numel())
 
 # Training loop
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr= 1)
+optimizer = optim.SGD(model.parameters(), lr= 0.9)
 
 
 train_losses_fGS = []
@@ -259,7 +259,7 @@ for X, y in test_loader:
     break
 
 
-# Define the model Not with Momemtum
+## SGD
 class LeNet5_SGD(nn.Module):
     def __init__(self):
         super(LeNet5_SGD, self).__init__()
@@ -340,7 +340,7 @@ for epoch in range(n_epochs):
     test_loss_SGD /= len(test_loader)
     test_losses.append((step, test_loss_SGD))
     print(f'Baseline SGD Test set: Average loss: {test_loss_SGD}, Train set: {train_loss_SGD} \
-        Accuracy: {correct}/{len(test_loader)} ({100. * correct / len(test_loader.dataset)}%)')
+        Accuracy: {correct}/{len(test_loader.dataset)} ({100. * correct / len(test_loader.dataset)}%)')
   
 
 
@@ -383,3 +383,106 @@ plt.tight_layout()
 plt.savefig('SGDvsfullSGD.png', bbox_inches='tight')
 plt.show()
 
+
+# Epoch 0, Step 12000, Loss: 0.18764866888523102
+# Mini SGD Test set: Average loss: 0.4333452764590075, Train set: 0.6592818779537144         Accuracy: 8429/10000 (84.29000091552734%)
+# Epoch 1, Step 24000, Loss: 0.7720698714256287
+# Mini SGD Test set: Average loss: 0.38226827884175973, Train set: 0.3883578435268913         Accuracy: 8601/10000 (86.01000213623047%)
+# Epoch 2, Step 36000, Loss: 0.5037024021148682
+# Mini SGD Test set: Average loss: 0.3630763161334263, Train set: 0.3390231965318186         Accuracy: 8674/10000 (86.73999786376953%)
+# Epoch 3, Step 48000, Loss: 0.02052803710103035
+# Mini SGD Test set: Average loss: 0.3298624603991296, Train set: 0.31097554669257566         Accuracy: 8815/10000 (88.1500015258789%)
+# Epoch 4, Step 60000, Loss: 0.18592920899391174
+# Mini SGD Test set: Average loss: 0.3183241964905125, Train set: 0.2914770603387345         Accuracy: 8835/10000 (88.3499984741211%)
+# Epoch 5, Step 72000, Loss: 0.33072593808174133
+# Mini SGD Test set: Average loss: 0.3076916441890844, Train set: 0.27521272502440114         Accuracy: 8905/10000 (89.05000305175781%)
+# Epoch 6, Step 84000, Loss: 0.44478192925453186
+# Mini SGD Test set: Average loss: 0.29677916104626506, Train set: 0.2624731833848273         Accuracy: 8922/10000 (89.22000122070312%)
+# Epoch 7, Step 96000, Loss: 0.9116008877754211
+# Mini SGD Test set: Average loss: 0.30449887127823133, Train set: 0.2508949779036011         Accuracy: 8890/10000 (88.9000015258789%)
+# Epoch 8, Step 108000, Loss: 0.08856727182865143
+# Mini SGD Test set: Average loss: 0.2885714533507444, Train set: 0.24027986404110682         Accuracy: 8980/10000 (89.80000305175781%)
+# Epoch 9, Step 120000, Loss: 0.021943364292383194
+# Mini SGD Test set: Average loss: 0.28829763478534237, Train set: 0.23117498861937322         Accuracy: 8975/10000 (89.75%)
+# number of training samples: 60000
+# number of testing samples: 10000
+# datatype of the 1st training sample:  torch.FloatTensor
+# size of the 1st training sample:  torch.Size([1, 28, 28])
+# Shape of X [N, C, H, W]: torch.Size([64, 1, 28, 28])
+# Shape of y: torch.Size([64]) torch.int64
+# +--------------+------------+
+# |   Modules    | Parameters |
+# +--------------+------------+
+# | conv1.weight |    150     |
+# |  conv1.bias  |     6      |
+# | conv2.weight |    2400    |
+# |  conv2.bias  |     16     |
+# |  fc1.weight  |   30720    |
+# |   fc1.bias   |    120     |
+# |  fc2.weight  |   10080    |
+# |   fc2.bias   |     84     |
+# |  fc3.weight  |    840     |
+# |   fc3.bias   |     10     |
+# +--------------+------------+
+# Total Trainable Params: 44426
+# 30720
+# 10080
+# Epoch 0, Step 1, Loss: 2.3046462535858154
+# full batch size SGD Test set: Average loss: 2.2898607223656526, Train set: 2.3046462535858154         Accuracy: 1000/10000 (10.0%)
+# Epoch 1, Step 2, Loss: 2.2895519733428955
+# full batch size SGD Test set: Average loss: 2.2732411858382497, Train set: 2.2895519733428955         Accuracy: 1824/10000 (18.239999771118164%)
+# Epoch 2, Step 3, Loss: 2.2727773189544678
+# full batch size SGD Test set: Average loss: 2.2425775421652823, Train set: 2.2727773189544678         Accuracy: 2985/10000 (29.850000381469727%)
+# Epoch 3, Step 4, Loss: 2.241830825805664
+# full batch size SGD Test set: Average loss: 2.179340976059057, Train set: 2.241830825805664         Accuracy: 3321/10000 (33.209999084472656%)
+# Epoch 4, Step 5, Loss: 2.1781065464019775
+# full batch size SGD Test set: Average loss: 2.03864710346149, Train set: 2.1781065464019775         Accuracy: 4487/10000 (44.869998931884766%)
+# Epoch 5, Step 6, Loss: 2.036863327026367
+# full batch size SGD Test set: Average loss: 1.7564407670573823, Train set: 2.036863327026367         Accuracy: 4682/10000 (46.81999969482422%)
+# Epoch 6, Step 7, Loss: 1.7546576261520386
+# full batch size SGD Test set: Average loss: 1.8138067243964808, Train set: 1.7546576261520386         Accuracy: 3139/10000 (31.389999389648438%)
+# Epoch 7, Step 8, Loss: 1.80947744846344
+# full batch size SGD Test set: Average loss: 2.5322526093501194, Train set: 1.80947744846344         Accuracy: 945/10000 (9.449999809265137%)
+# Epoch 8, Step 9, Loss: 2.5304126739501953
+# full batch size SGD Test set: Average loss: 2.394351989600309, Train set: 2.5304126739501953         Accuracy: 1768/10000 (17.68000030517578%)
+# Epoch 9, Step 10, Loss: 2.3963096141815186
+# full batch size SGD Test set: Average loss: 2.11471440457994, Train set: 2.3963096141815186         Accuracy: 1604/10000 (16.040000915527344%)
+# Shape of X [N, C, H, W]: torch.Size([64, 1, 28, 28])
+# Shape of y: torch.Size([64]) torch.int64
+# +--------------+------------+
+# |   Modules    | Parameters |
+# +--------------+------------+
+# | conv1.weight |    150     |
+# |  conv1.bias  |     6      |
+# | conv2.weight |    2400    |
+# |  conv2.bias  |     16     |
+# |  fc1.weight  |   30720    |
+# |   fc1.bias   |    120     |
+# |  fc2.weight  |   10080    |
+# |   fc2.bias   |     84     |
+# |  fc3.weight  |    840     |
+# |   fc3.bias   |     10     |
+# +--------------+------------+
+# Total Trainable Params: 44426
+# 30720
+# 10080
+# Epoch 0, Step 938, Loss: 0.7700589895248413
+# Baseline SGD Test set: Average loss: 0.5581217858062428, Train set: 0.7361105777847488         Accuracy: 7838/157 (78.37999725341797%)
+# Epoch 1, Step 1876, Loss: 0.48054784536361694
+# Baseline SGD Test set: Average loss: 0.5164653434874905, Train set: 0.43128219543934376         Accuracy: 7967/157 (79.66999816894531%)
+# Epoch 2, Step 2814, Loss: 0.3495858609676361
+# Baseline SGD Test set: Average loss: 0.3773086298802856, Train set: 0.3686265293627914         Accuracy: 8616/157 (86.16000366210938%)
+# Epoch 3, Step 3752, Loss: 0.09479450434446335
+# Baseline SGD Test set: Average loss: 0.34974200017512985, Train set: 0.3354985183363022         Accuracy: 8731/157 (87.30999755859375%)
+# Epoch 4, Step 4690, Loss: 0.5822596549987793
+# Baseline SGD Test set: Average loss: 0.35362001418308087, Train set: 0.3130413601790537         Accuracy: 8702/157 (87.0199966430664%)
+# Epoch 5, Step 5628, Loss: 0.1348230093717575
+# Baseline SGD Test set: Average loss: 0.32878074908901933, Train set: 0.29516055106099987         Accuracy: 8777/157 (87.7699966430664%)
+# Epoch 6, Step 6566, Loss: 0.5123618841171265
+# Baseline SGD Test set: Average loss: 0.3187098038519264, Train set: 0.281045419773631         Accuracy: 8851/157 (88.51000213623047%)
+# Epoch 7, Step 7504, Loss: 0.22133305668830872
+# Baseline SGD Test set: Average loss: 0.32485921078237, Train set: 0.27044916211732667         Accuracy: 8818/157 (88.18000030517578%)
+# Epoch 8, Step 8442, Loss: 0.1285751760005951
+# Baseline SGD Test set: Average loss: 0.3124188157688281, Train set: 0.26017047404480387         Accuracy: 8848/157 (88.4800033569336%)
+# Epoch 9, Step 9380, Loss: 0.24161110818386078
+# Baseline SGD Test set: Average loss: 0.30183710803271857, Train set: 0.25069897447917255         Accuracy: 8869/157 (88.69000244140625%)
---
+++ asg1/data/iterate_avg.py
@@ -0,0 +1,202 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms, utils
+#import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from prettytable import PrettyTable
+
+#Global epochs
+n_epochs = 10
+
+seed = 42
+torch.manual_seed(seed)
+#random.seed(seed)
+np.random.seed(seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+# Download the MNIST dataset
+transform = transforms.ToTensor()
+train_dataset = datasets.FashionMNIST(root='./data', train=True, download=True, transform=transform)
+test_dataset = datasets.FashionMNIST(root='./data', train=False, download=True, transform=transform)
+
+print("number of training samples: " + str(len(train_dataset)) + "\n" +
+      "number of testing samples: " + str(len(test_dataset)))
+
+print("datatype of the 1st training sample: ", train_dataset[0][0].type())
+print("size of the 1st training sample: ", train_dataset[0][0].size())
+#Taking a average base size
+batch_size = 64
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+
+
+## SGD
+class LeNet5_SGD(nn.Module):
+    def __init__(self):
+        super(LeNet5_SGD, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=5, stride=1)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5, stride=1)
+        self.fc1 = nn.Linear(256, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = torch.tanh(self.conv1(x))
+        x = torch.max_pool2d(x, kernel_size=2, stride=2)
+        x = torch.tanh(self.conv2(x))
+        x = torch.max_pool2d(x, kernel_size=2, stride=2)
+        x = x.view(-1, 256)
+        x = torch.tanh(self.fc1(x))
+        x = torch.tanh(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+def count_parameters(model):
+    table = PrettyTable(["Modules", "Parameters"])
+    total_params = 0
+    for name, parameter in model.named_parameters():
+        if not parameter.requires_grad:
+            continue
+        params = parameter.numel()
+        table.add_row([name, params])
+        total_params += params
+    print(table)
+    print(f"Total Trainable Params: {total_params}")
+    return total_params
+
+model = LeNet5_SGD()
+
+count_parameters(model)
+fc12_params = [p for name, p in model.named_parameters() if name in ['fc1.weight', 'fc2.weight']]
+print(fc12_params[0].numel())
+print(fc12_params[1].numel())
+
+# raise SystemExit
+
+# Training loop
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr= 0.35) #Intilize learning rate
+swa_model  = optim.swa_utils.AveragedModel(model) #Averaging model weights pr. iteration
+scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer,T_max=10) #It will change learning rate each time 
+
+
+train_losses = []
+test_losses = []
+
+step = 0
+for epoch in range(n_epochs):
+    model.train()
+    train_loss_SGD = 0
+    for i, (images, labels) in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = model(images)
+        loss_SGD = criterion(output, labels)
+        train_loss_SGD += criterion(output, labels).item()
+        loss_SGD.backward()
+        optimizer.step()
+        step += 1
+    scheduler.step()
+    swa_model.update_parameters(model)
+    train_loss_SGD /= len(train_loader)
+    train_losses.append((step, train_loss_SGD))
+    print(f'Epoch {epoch}, Step {step}, Loss: {loss_SGD.item()}')
+
+    model.eval()
+    test_loss_SGD = 0
+    correct = 0
+    with torch.no_grad():
+        for images, labels in test_loader:
+            output = model(images)
+            test_loss_SGD += criterion(output, labels).item()
+            pred = torch.argmax(output, dim=1)
+            correct += pred.eq(labels).sum()
+
+    test_loss_SGD /= len(test_loader)
+    test_losses.append((step, test_loss_SGD))
+    print(f'Baseline SGD Test set: Average loss: {test_loss_SGD}, Train set: {train_loss_SGD} \
+        Accuracy: {correct}/{len(test_loader.dataset)} ({100. * correct / len(test_loader.dataset)}%)')
+  
+
+
+
+
+
+#  plot train and test losses for SGD Baseline
+train_steps, train_loss_SGD = zip(*train_losses)
+test_steps, test_loss_SGD = zip(*test_losses)
+
+
+plt.plot(range(1,n_epochs+1), test_loss_SGD, label='Test Loss  SGD', linestyle='--', color = 'blue')
+plt.plot(range(1,n_epochs+1), train_loss_SGD, label='Training Loss SGD',color='blue')
+
+
+plt.title(' SGD Exponential decreasing learning rate:Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.grid(True)
+#Creating a box that isn't too large
+plt.tight_layout()
+plt.savefig('SGD_iteatemethod.png', bbox_inches='tight')
+plt.show()
+
+
+
+
+
+# Create data loaders.
+
+
+# Epoch 0, Step 1, Loss: 2.2992544174194336
+# full batch size SGD Test set: Average loss: 2.2760017191528514, Train set: 2.2992544174194336         Accuracy: 1144/10000 (11.4399995803833%)
+# Epoch 1, Step 2, Loss: 2.2758026123046875
+# full batch size SGD Test set: Average loss: 2.2433934561006583, Train set: 2.2758026123046875         Accuracy: 3138/10000 (31.3799991607666%)
+# Epoch 2, Step 3, Loss: 2.2430551052093506
+# full batch size SGD Test set: Average loss: 2.1894149977690094, Train set: 2.2430551052093506         Accuracy: 3585/10000 (35.849998474121094%)
+# Epoch 3, Step 4, Loss: 2.1889073848724365
+# full batch size SGD Test set: Average loss: 2.101333569569193, Train set: 2.1889073848724365         Accuracy: 4402/10000 (44.02000045776367%)
+# Epoch 4, Step 5, Loss: 2.1006367206573486
+# full batch size SGD Test set: Average loss: 1.967674787636775, Train set: 2.1006367206573486         Accuracy: 4718/10000 (47.18000030517578%)
+# Epoch 5, Step 6, Loss: 1.9668816328048706
+# full batch size SGD Test set: Average loss: 1.8115000550154667, Train set: 1.9668816328048706         Accuracy: 4752/10000 (47.52000045776367%)
+# Epoch 6, Step 7, Loss: 1.8107725381851196
+# full batch size SGD Test set: Average loss: 1.6900820193017365, Train set: 1.8107725381851196         Accuracy: 4867/10000 (48.66999816894531%)
+# Epoch 7, Step 8, Loss: 1.6894973516464233
+# full batch size SGD Test set: Average loss: 1.623711497920334, Train set: 1.6894973516464233         Accuracy: 4961/10000 (49.61000061035156%)
+# Epoch 8, Step 9, Loss: 1.6232185363769531
+# full batch size SGD Test set: Average loss: 1.5959555457352073, Train set: 1.6232185363769531         Accuracy: 4994/10000 (49.939998626708984%)
+# Epoch 9, Step 10, Loss: 1.5954980850219727
+# full batch size SGD Test set: Average loss: 1.589197133756747, Train set: 1.5954980850219727         Accuracy: 5005/10000 (50.04999923706055%)
+# Training loop
+# criterion = nn.CrossEntropyLoss()
+# optimizer = optim.SGD(model.parameters(), lr= 0.9) #Intilize learning rate
+# swa_model  = optim.swa_utils.AveragedModel(model) #Averaging model weights pr. iteration
+# scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer,T_max=10) #It will change learning rate each time
+
+
+# Epoch 0, Step 938, Loss: 0.455269455909729
+# Baseline SGD Test set: Average loss: 0.44186134180825226, Train set: 0.5741125271518601         Accuracy: 8385/10000 (83.8499984741211%)
+# Epoch 1, Step 1876, Loss: 0.34925949573516846
+# Baseline SGD Test set: Average loss: 0.38076501107139954, Train set: 0.3621360699552844         Accuracy: 8583/10000 (85.83000183105469%)
+# Epoch 2, Step 2814, Loss: 0.21631628274917603
+# Baseline SGD Test set: Average loss: 0.3425143601693166, Train set: 0.31407840213160526         Accuracy: 8728/10000 (87.27999877929688%)
+# Epoch 3, Step 3752, Loss: 0.4887586236000061
+# Baseline SGD Test set: Average loss: 0.33331062744377526, Train set: 0.2789913438705366         Accuracy: 8777/10000 (87.7699966430664%)
+# Epoch 4, Step 4690, Loss: 0.18647176027297974
+# Baseline SGD Test set: Average loss: 0.28906063336855287, Train set: 0.2512660244825298         Accuracy: 8947/10000 (89.47000122070312%)
+# Epoch 5, Step 5628, Loss: 0.2354133129119873
+# Baseline SGD Test set: Average loss: 0.29549975005114915, Train set: 0.22797275744060844         Accuracy: 8931/10000 (89.30999755859375%)
+# Epoch 6, Step 6566, Loss: 0.28161054849624634
+# Baseline SGD Test set: Average loss: 0.2752798169281832, Train set: 0.2097631217296253         Accuracy: 9001/10000 (90.01000213623047%)
+# Epoch 7, Step 7504, Loss: 0.11021572351455688
+# Baseline SGD Test set: Average loss: 0.2738002862330455, Train set: 0.19840931143365434         Accuracy: 9024/10000 (90.23999786376953%)
+# Epoch 8, Step 8442, Loss: 0.14891542494297028
+# Baseline SGD Test set: Average loss: 0.2738002862330455, Train set: 0.19533936651562578         Accuracy: 9024/10000 (90.23999786376953%)
+# Epoch 9, Step 9380, Loss: 0.19648505747318268
+# Baseline SGD Test set: Average loss: 0.2738891867980076, Train set: 0.196624319625101         Accuracy: 9017/10000 (90.16999816894531%)
